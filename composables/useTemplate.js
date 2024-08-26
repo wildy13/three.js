cfd392d449b4fe93e95199b9d40408e5dff2a6b9
wildy13 <wildy.simanjuntak13@gmail.com>
2024-08-26 15:13:53 +0700

    update

---
+++ composables/useTemplate.js
@@ -0,0 +1,78 @@
+import * as THREE from 'three';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+import { BoxLineGeometry } from 'three/examples/jsm/geometries/BoxLineGeometry.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+export const useTemplate = () => {
+    let initScene, initRenderer, initCamera, initControls;
+    let initRoom;
+    let initRaycaster;
+    let initControler1, initController2;
+
+    // raycaster
+
+    initRaycaster = new THREE.Raycaster();
+
+    // Scene
+    initScene = new THREE.Scene();
+    initScene.background = new THREE.Color(0x505050);
+
+    // Cahaya
+    initScene.add(new THREE.HemisphereLight(0xa5a5a5, 0x898989, 3));
+    const light = new THREE.DirectionalLight(0xffffff, 3);
+    light.position.set(1, 1, 1).normalize();
+    initScene.add(light);
+
+    // Kamera
+    initCamera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
+    initCamera.position.set(0, 1, 3);
+
+    // Ruangan
+    initRoom = new THREE.LineSegments(
+        new BoxLineGeometry(6, 6, 6, 10, 10, 10).translate(0, 3, 0),
+        new THREE.LineBasicMaterial({ color: 0xbcbcbc })
+    );
+    initScene.add(initRoom);
+
+    // Renderer
+    initRenderer = new THREE.WebGLRenderer({ antialias: true });
+    initRenderer.setPixelRatio(window.devicePixelRatio);
+    initRenderer.setSize(window.innerWidth, window.innerHeight);
+    initRenderer.xr.enabled = true;
+
+    // Kontrol orbit
+    initControls = new OrbitControls(initCamera, initRenderer.domElement);
+    initControls.target = new THREE.Vector3(0, 1.2, -1);
+    initControls.update();
+
+    // Kontroler VR
+    initControler1 = initRenderer.xr.getController(0); // Tangan kiri
+    initController2 = initRenderer.xr.getController(1); // Tangan kanan
+
+    initScene.add(initControler1);
+    initScene.add(initController2);
+
+
+    function _create(Container) {
+        // VR
+        const sessionInit = {
+            requiredFeatures: ['hand-tracking']
+        };
+
+        Container.appendChild(initRenderer.domElement);
+        Container.appendChild(VRButton.createButton(initRenderer, sessionInit));
+    }
+
+
+    return {
+        _create,
+        initScene,
+        initRenderer,
+        initCamera,
+        initControls,
+        initRoom,
+        initRaycaster,
+        initControler1,
+        initController2
+    }
+}
--- pages/vr.vue
+++ pages/vr.vue
@@ -1,9 +1,10 @@
 <script setup>
 import * as THREE from 'three';
-import { BoxLineGeometry } from 'three/examples/jsm/geometries/BoxLineGeometry.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+
+
 import { onMounted, ref } from 'vue';
+import { useTemplate } from '../composables/useTemplate';
+import { useHand } from '../composables/useHand';
 
 
 
@@ -16,64 +17,44 @@ let raycaster;
 let controller1, controller2;
 
 const { _hand, update } = useHand();
-raycaster = new THREE.Raycaster();
+
 
 onMounted(() => {
+    const {
+        _create,
+        initScene,
+        initRenderer,
+        initCamera,
+        initControls,
+        initRoom,
+        initRaycaster,
+        initControler1,
+        initController2
+    } = useTemplate();
+
+    scene = initScene,
+        renderer = initRenderer,
+        camera = initCamera,
+        controls = initControls,
+        room = initRoom,
+        raycaster = initRaycaster,
+        controller1 = initControler1,
+        controller2 = initController2
+
+    // template
+    _create(Container.value);
+
+    // hand
+    _hand(scene, renderer, controller1, controller2);
+
     _init();
     animate();
 });
 
 
 function _init() {
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x505050);
 
-    // Cahaya
-    scene.add(new THREE.HemisphereLight(0xa5a5a5, 0x898989, 3));
-    const light = new THREE.DirectionalLight(0xffffff, 3);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
 
-    // Kamera
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
-    camera.position.set(0, 1, 3);
-
-    // Ruangan
-    room = new THREE.LineSegments(
-        new BoxLineGeometry(6, 6, 6, 10, 10, 10).translate(0, 3, 0),
-        new THREE.LineBasicMaterial({ color: 0xbcbcbc })
-    );
-    scene.add(room);
-
-    // Renderer
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.xr.enabled = true;
-
-    // VR
-    const sessionInit = {
-        requiredFeatures: ['hand-tracking']
-    };
-
-    Container.value.appendChild(renderer.domElement);
-    Container.value.appendChild(VRButton.createButton(renderer, sessionInit));
-
-    // Kontrol orbit
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.target = new THREE.Vector3(0, 1.2, -1);
-    controls.update();
-
-    // Kontroler VR
-    controller1 = renderer.xr.getController(0); // Tangan kiri
-    controller2 = renderer.xr.getController(1); // Tangan kanan
-
-    // Tambahkan kontroler ke scene
-    scene.add(controller1);
-    scene.add(controller2);
-
-    // hand
-    _hand(scene, renderer, controller1, controller2);
 
     // Cube
     const geometry = new THREE.BoxGeometry();
